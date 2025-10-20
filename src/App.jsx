82e385347a8e5b3e95b075d2030d75ee2c556f9e
fedebacci <federicobacci1999@gmail.com>
2025-10-20 11:56:27 +0200

    dotenv installation and basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
+const appName = import.meta.env.VITE_APP_NAME;
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      {appName}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
