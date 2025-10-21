dc7a66dbd9d3b17f1735de6b0a76ac55fbffbbe8
fedebacci <federicobacci1999@gmail.com>
2025-10-21 19:42:16 +0200

    test (not working) of making navbar collapse on small screens, left commented

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -14,62 +14,71 @@ export default function Header () {
                             {import.meta.env.VITE_APP_NAME}
                         </span>
                     </Link>
-                    <ul className="nav nav-pills">
-                        <li className="nav-item">
-                            <NavLink className='nav-link' to={pages.HOME()}>
-                                Home
-                            </NavLink>
-                        </li>
-                        {/* <li className="nav-item">
-                            <NavLink className='nav-link' to={pages.GAMES()}>
-                                Games
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className='nav-link' to={pages.CARDS()}>
-                                Cards
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className='nav-link' to={pages.DECKS()}>
-                                Decks
-                            </NavLink>
-                        </li> */}
 
 
-                        <li className="nav-item dropdown">
-                            <a id="navbarDropdown" className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    Resources
-                            </a>
+                    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span className="navbar-toggler-icon"></span>
+                    </button> */}
 
-                            <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <NavLink className="dropdown-item" to={pages.GAMES()}>
+
+                    {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
+                        <ul className="nav nav-pills">
+                            <li className="nav-item">
+                                <NavLink className='nav-link' to={pages.HOME()}>
+                                    Home
+                                </NavLink>
+                            </li>
+                            {/* <li className="nav-item">
+                                <NavLink className='nav-link' to={pages.GAMES()}>
                                     Games
                                 </NavLink>
-                                <NavLink className="dropdown-item" to={pages.CARDS()}>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className='nav-link' to={pages.CARDS()}>
                                     Cards
                                 </NavLink>
-                                <NavLink className="dropdown-item" to={pages.DECKS()}>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className='nav-link' to={pages.DECKS()}>
                                     Decks
                                 </NavLink>
-                            </div>
-                        </li>
+                            </li> */}
+
+
+                            <li className="nav-item dropdown">
+                                <a id="navbarDropdown" className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        Resources
+                                </a>
+
+                                <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <NavLink className="dropdown-item" to={pages.GAMES()}>
+                                        Games
+                                    </NavLink>
+                                    <NavLink className="dropdown-item" to={pages.CARDS()}>
+                                        Cards
+                                    </NavLink>
+                                    <NavLink className="dropdown-item" to={pages.DECKS()}>
+                                        Decks
+                                    </NavLink>
+                                </div>
+                            </li>
 
 
 
 
 
-                        {/* <li className="nav-item">
-                            <NavLink className='nav-link' to="/test">
-                                Test 404
-                            </NavLink>
-                        </li> */}
-                        <li className="nav-item">
-                            <NavLink className='nav-link' target="_blank" to={import.meta.env.VITE_BACKOFFICE_URL}>
-                                Backoffice (Login) <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                            </NavLink>
-                        </li>
-                    </ul>
+                            {/* <li className="nav-item">
+                                <NavLink className='nav-link' to="/test">
+                                    Test 404
+                                </NavLink>
+                            </li> */}
+                            <li className="nav-item">
+                                <NavLink className='nav-link' target="_blank" to={import.meta.env.VITE_BACKOFFICE_URL}>
+                                    Backoffice (Login) <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                                </NavLink>
+                            </li>
+                        </ul>
+                    {/* </div> */}
                 </header>
             </div>
         </section>
