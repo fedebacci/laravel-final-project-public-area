03bc8f4811c83b9060bc59af102df9dcd6774577
fedebacci <federicobacci1999@gmail.com>
2025-10-28 20:17:44 +0100

    basic setup of homepage for showing custom filtered images (still all resources are separated, no component in commone)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,162 @@
-// import ResourcesList from "../components/resources/ResourcesList";
+import { useState, useEffect } from "react";
+import { useLoader } from "../contexts/LoaderContext";
+import axios from "axios";
+const apiUrl = import.meta.env.VITE_API_URL;
+import { Link } from "react-router-dom";
+import pages from "../assets/js/pages";
 
-// import { useResources } from "../contexts/ResourcesContext";
+const gamesFilters = {
+    // id: 1,
+    name: "pok",
+};
+const cardsFilters = {
+    name: "char",
+    description: "phase",
+    // image: true,
+};
+const decksFilters = {
+    name: "pok",
+};
 
 export default function HomePage () {
     console.debug('⚙️ LOADING HOME .......... .......... .......... .......... .......... .......... .......... .......... .......... .......... .......... .......... .......... .......... ..........');
-    // const { homeGames, homeCards, homeDecks } = useResources();
-    // console.debug('⚙️homeGames', homeGames);
-    // console.debug('⚙️homeCards', homeCards);
-    // console.debug('⚙️homeDecks', homeDecks);
+
+    const { setIsLoading } = useLoader();
+    const [games, setGames] = useState([]);
+    const [cards, setCards] = useState([]);
+    const [decks, setDecks] = useState([]);
+
+
+
+    useEffect(() => {
+        fetchGames(apiUrl + 'games', gamesFilters); 
+        fetchCards(apiUrl + 'cards', cardsFilters); 
+        fetchDecks(apiUrl + 'decks', decksFilters); 
+    }, []);
+
+
+
+
+
+
+
+    function fetchGames (requestUrl, filters = null) {
+        
+        const urlFilters = new URLSearchParams(filters);
+        requestUrl = requestUrl + '?' + urlFilters;
+        console.info('requestUrl', requestUrl);
+        
+        setIsLoading(true);
+        axios
+            .get(`${requestUrl}`)
+            .then(response => {
+                console.info(response.data);
+                const TMPgames = response.data.data.map((game) => {
+                    return {
+                        id: game.id,
+                        image: game.logo,
+                        name: game.name,
+                        description: game.description,
+                    }
+                });
+                console.info('TMPgames', TMPgames); 
+                setGames(TMPgames);
+            })
+            .catch(error => {
+                console.error(`new error on request ${requestUrl}`);
+                console.error(error);
+                setGames([]);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function fetchCards (requestUrl, filters = null) {
+
+        const urlFilters = new URLSearchParams(filters);
+        requestUrl = requestUrl + '?' + urlFilters;
+        console.info('requestUrl', requestUrl);
+
+        setIsLoading(true);
+        axios
+            .get(`${requestUrl}`)
+            .then(response => {
+                console.info(response.data);
+                const TMPcards = response.data.data.map((card) => {
+                    return {
+                        id: card.id,
+                        image: card.image,
+                        name: card.name,
+                        description: card.description,
+                    }
+                });
+                console.info('TMPcards', TMPcards);
+                setCards(TMPcards);
+            })
+            .catch(error => {
+                console.error(`new error on request ${requestUrl}`);
+                console.error(error);
+                setCards([]);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }    
+
+
+
+
+
+
+
+
+
+
+
+    function fetchDecks (requestUrl, filters = null) {
+
+        const urlFilters = new URLSearchParams(filters);
+        requestUrl = requestUrl + '?' + urlFilters;
+        console.info('requestUrl', requestUrl);
+
+        setIsLoading(true);
+        axios
+            .get(`${requestUrl}`)
+            .then(response => {
+                console.info(response.data);
+                const TMPdecks = response.data.data.map((deck) => {
+                    return {
+                        id: deck.id,
+                        name: deck.name,
+                        description: deck.description,
+                    }
+                });
+                console.info('TMPdecks', TMPdecks);
+                setDecks(TMPdecks);
+            })
+            .catch(error => {
+                console.error(`new error on request ${requestUrl}`);
+                console.error(error);
+                setDecks([]);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }  
+
 
     return (
         <section id="homepage-content">
@@ -16,11 +165,198 @@ export default function HomePage () {
                     HOMEPAGE - {import.meta.env.VITE_APP_NAME}
                 </h2>
 
-                {/* <ResourcesList resourceType={'games'} isHome="true"/>
-                <ResourcesList resourceType={'cards'} isHome="true"/>
-                <ResourcesList resourceType={'decks'} isHome="true"/> */}
-                <hr />
-                {/* <ResourcesList resourceType={'games'}/> */}
+
+
+
+
+
+                {
+                    games.length == 0 ?
+                        <div className="my-5">
+                            <h4>
+                                games Home
+                            </h4>
+                            <p className="alert alert-warning m-0">
+                                No games have been found.
+                            </p>
+                        </div>
+                    :
+                        <div className="my-5">
+                            <h4>
+                                games Home
+                            </h4>
+                            <div className="row g-3">
+                                {
+                                    games.map(game => {
+                                        return (
+                                            <div className="col-12 col-md-4 col-lg-3" key={game.id}>
+                                                <div className="card h-100">
+                                                    <div className="card-body">
+                                                        <h3>
+                                                            {game.name}
+                                                        </h3>
+                                                        {/* {game.name} */}
+                                                        <p>
+                                                            {
+                                                                game.description != null && game.description.length > 50 ?
+                                                                    game.description.slice(0,50) + '...'
+                                                                :
+                                                                    game.description != null ?
+                                                                        game.description
+                                                                    :
+                                                                        'No description'
+                                                            }
+                                                        </p>
+                                                        <div className="mb-3">
+                                                            <Link to={pages.SHOWGAME(game.id)} className="text-decoration-none">
+                                                                Show
+                                                            </Link>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        );
+                                    })
+                                }
+                            </div>
+                        </div>             
+                }   
+
+
+
+
+
+
+
+
+
+
+
+
+
+                {
+                    cards.length == 0 ?
+                        <div className="my-5">
+                            <h4>
+                                cards Home
+                            </h4>
+                            <div className="mb-3">
+                                <p className="alert alert-warning m-0">
+                                    No cards have been found.
+                                </p>
+                            </div>
+                        </div>
+                    :
+                        <div className="my-5">
+                            <h4>
+                                cards Home
+                            </h4>
+                            <div className="row g-3">
+                                {
+                                    cards.map(card => {
+                                        return (
+                                            <div className="col-12 col-md-4 col-lg-3" key={card.id}>
+                                                <div className="card h-100">
+                                                    <div className="card-body">
+                                                        <h3>
+                                                            {card.name}
+                                                        </h3>
+                                                        {/* {card.name} */}
+                                                        <p>
+                                                            {
+                                                                card.description != null && card.description.length > 50 ?
+                                                                    card.description.slice(0,50) + '...'
+                                                                :
+                                                                    card.description != null ?
+                                                                        card.description
+                                                                    :
+                                                                        'No description'
+                                                            }
+                                                        </p>
+                                                        <div className="mb-3">
+                                                            <Link to={pages.SHOWCARD(card.id)} className="text-decoration-none">
+                                                                Show
+                                                            </Link>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        );
+                                    })
+                                }
+                            </div>
+                        </div>                    
+                } 
+
+
+
+
+
+
+
+
+
+
+                {
+                    decks.length == 0 ?
+                        <div className="my-5">
+                            <h4>
+                                decks Home
+                            </h4>
+                            <p className="alert alert-warning m-0">
+                                No decks have been found.
+                            </p>
+                        </div>
+                    :
+                        <div className="my-5">
+                            <h4>
+                                decks Home
+                            </h4>
+                            <div className="row g-3">
+                                {
+                                    decks.map(deck => {
+                                        return (
+                                            <div className="col-12 col-md-4 col-lg-3" key={deck.id}>
+                                                <div className="card h-100">
+                                                    <div className="card-body">
+                                                        <h3>
+                                                            {deck.name}
+                                                        </h3>
+                                                        {/* {deck.name} */}
+                                                        <p>
+                                                            {
+                                                                deck.description != null && deck.description.length > 50 ?
+                                                                    deck.description.slice(0,50) + '...'
+                                                                :
+                                                                    deck.description != null ?
+                                                                        deck.description
+                                                                    :
+                                                                        'No description'
+                                                            }
+                                                        </p>
+                                                        <div className="mb-3">
+                                                            <Link to={pages.SHOWDECK(deck.id)} className="text-decoration-none">
+                                                                Show
+                                                            </Link>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        );
+                                    })
+                                }
+                            </div>
+                        </div>
+
+                    
+                } 
+
+
+
+
+
+
+
             </div>
         </section>
     );
